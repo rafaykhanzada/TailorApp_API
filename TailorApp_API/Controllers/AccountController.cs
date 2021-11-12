2f665193af32024d64a8ca021b52b116dd8c2bd0
rafay_khan <rafaykhan3012@hotmail.com>
2021-11-12 16:43:20 +0500

    Signup

---
+++ TailorApp_API/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TailorApp_API.DataContext;
+using TailorApp_API.Models;
+using TailorApp_API.Repository;
+
+namespace TailorApp_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost("signup")]
+        public async Task<IdentityResult> SignUp([FromBody] SignUpModel model)
+        {
+            var user = new User()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Address = model.Address,
+                Country = model.Country,
+                Email = model.Email,
+                UserName = model.Email
+            };
+            var result = await _userManager.CreateAsync(user,model.Password);
+            if (result.Succeeded)
+            {
+                return result;
+            }
+            return null;
+        }
+        [HttpGet("getdata")]
+        public string getdata()
+        {
+            return "heelo";
+        }
+    }
+}
---
+++ TailorApp_API/Models/SignUpModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TailorApp_API.Models
+{
+    public class SignUpModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+    }
+}
