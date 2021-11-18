f39423baa4066b55643878da84b363aad96214ff
rafay_khan <rafaykhan3012@hotmail.com>
2021-11-18 20:16:35 +0500

    MiddleWear

---
+++ TailorApp_API/Middlewears/Cookie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TailorApp_API.Middlewears
+{
+    public class Cookie
+    {
+    }
+}
